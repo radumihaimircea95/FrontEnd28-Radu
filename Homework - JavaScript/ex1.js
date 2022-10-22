e29a4d61053d3b6cb5d3f20dff266aa8782a346a
Radu Mircea <mircearadumihai@gmail.com>
2022-10-22 12:33:40 +0300

    update

--- Homework - JavaScript/ex1.js
+++
@@ -1,9 +0,0 @@
-//display in the console the numbers from 1 to 20
-for (let n = 1; n < 21; n++) {
-  console.log(n);
-}
-
-//display in the console the odd numbers from 1 to 20
-for (let i = 1; i < 21; i += 2) {
-  console.log(i);
-}
